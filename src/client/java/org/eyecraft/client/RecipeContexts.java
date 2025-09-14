cdb3ce1ec46ff4b982afb9c16f73efcf9bc03070
Troy Gunawardene <troy.hiruka@gmail.com>
2025-09-14 01:59:25 -0400

    very basic crafting mvp

---
+++ src/client/java/org/eyecraft/client/RecipeContexts.java
@@ -0,0 +1,12 @@
+package org.eyecraft.client;
+
+import net.minecraft.util.context.ContextParameterMap;
+import net.minecraft.util.context.ContextType;
+
+// Reusable empty context
+public final class RecipeContexts {
+  public static final ContextType EMPTY_TYPE =
+      new ContextType.Builder().build();                    // no required/allowed params
+  public static final ContextParameterMap EMPTY_CTX =
+      new ContextParameterMap.Builder().build(EMPTY_TYPE);  // empty map bound to empty type
+}
